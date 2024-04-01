778a28ca48f1f6da475f8b838f6be787a5279d92
Avdhut Pailwan <avdhutpailwan@gmail.com>
2024-04-01 11:09:23 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,67 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello World Ritesh Bakare");
 });
 
-let gazeDataStore = [];
+let gazeDataStore = {};
 
 app.post("/api/gaze", (req, res) => {
-  const gazeData = req.body.gazeData;
-  // console.log(`the data points are ${gazeData}`);
-  gazeData.forEach((point) => {
-    console.log(`x: ${point.x}, y: ${point.y}`);
-    gazeDataStore.push(point.x, point.y);
+  gazeDataStore = req.body.gazeData;
+  let intialGazeData = Object.keys(gazeDataStore);
+  const gazeData = intialGazeData.map((item) => {
+    let x = Number(item.split(",")[0]);
+    let y = Number(item.split(",")[1]);
+    let values = gazeDataStore[item];
+    return { x, y, values };
   });
+
+  // console.log(`the data points are ${gazeData}`);
+  gazeData.forEach((item) => {
+    console.log(`x : ${item['x']}, y : ${item['y']}, values : ${item['values']}`)
+  })
+  const keys = Object.keys(gazeData[0]).join(',') + '\n';
+  
+
+  const csvContent = [
+    ...gazeData.map((obj) => Object.values(obj).join(",")), // Map objects to data rows
+  ].join("\n");
+
+  if(fs.existsSync("./public/csvData.csv")){
+    fs.appendFileSync("./public/csvData.csv", '\n' + csvContent, function(err) {
+      if (err) throw err;
+      console.log("Saved!!!");
+    });
+  }else {
+    fs.appendFileSync("./public/csvData.csv", keys + csvContent, function(err) {
+      if (err) throw err;
+      console.log("Saved!!!");
+    });
+  }
+  
 
   res.status(200).json({ message: "Gaze data received successfully. " });
 });
 
-
+app.get("/api/gaze", (req, res) => {
+  let intialGazeData = Object.keys(gazeDataStore);
+  const gazeData = intialGazeData.map((item) => {
+    let x = Number(item.split(",")[0]);
+    let y = Number(item.split(",")[1]);
+    let values = gazeDataStore[item];
+    return { x, y, values };
+  });
+  res.sendFile(__dirname + '/public/csvData.csv');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/script.js
+++ frontend/script.js
@@ -1,66 +1,194 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let isTracking = false;
-  let isRecording = false;
+// let isTracking = false;
+// let isRecording = false;
+let visibleMap = false;
 
-  const startBtn = document.getElementById("startBtn");
-  const stopBtn = document.getElementById("stopBtn");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const showHeatMapButton = document.getElementById("seemap");
 
-  startBtn.addEventListener("click", startTracking);
-  stopBtn.addEventListener("click", stopTracking);
+startBtn.addEventListener("click", startTracking);
+stopBtn.addEventListener("click", stopTracking);
+showHeatMapButton.addEventListener("click", showHeatMap);
 
-  let gazeData = [];
+let gazeData = {};
 
-  function startTracking() {
-    isTracking = true;
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
-
-    webgazer
-      .setGazeListener(function (data, elapsedTime) {
-        console.log(data);
-        gazeData.push({ x: data.x, y: data.y });
-        if (isRecording && data && data.x !== null && data.y !== null) {
-          gazeDataStore.push({ x: data.x, y: data.y });
-        }
-      })
-      .begin();
-  }
-
-  function stopTracking() {
-    isTracking = false;
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-    webgazer.end();
-    console.log("Data Revied :-) \n\n");
-    console.log(gazeData);
-    sendDataToServer();
-  }
-
-  function sendDataToServer() {
-    
-    const url = "http://localhost:3000/api/gaze";
-
-    const data = { gazeData: gazeData };
-
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+function startTracking() {
+  // isTracking = true;
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  webgazer
+    .begin()
+    .then(function (results) {
+      console.log("WebGazer initialized successfully!");
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+    .catch(function (err) {
+      console.log("Error:", err);
+    });
+
+  if (webgazer.detectCompatibility()) {
+    webgazer.setGazeListener(function (data, elapsedTime) {
+      console.log(data);
+      console.log(elapsedTime);
+      if (
+        data &&
+        data.x !== null &&
+        data.y !== null &&
+        data.x > 0 &&
+        data.y > 0
+      ) {
+        let key = `${data.x},${data.y}`;
+        const gazeText = `Gaze: X :: ${data.x}, Y :: ${data.y}`;
+        document.getElementById("gaze-output").textContent = gazeText;
+        if (gazeData.hasOwnProperty(key)) {
+          gazeData[key] += 1; // Increment frequency if it exists
+        } else {
+          gazeData[key] = 1; // Add the point with frequency 1
         }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Server response:", data);
-        console.log("Data Recived Sucessfully ");
-      })
-      .catch((error) => {
-        console.error("Error during POST request:", error);
+      }
+    });
+  }
+}
+function stopTracking() {
+  // isTracking = false;
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  webgazer.end();
+  console.log("Data Revied :-) \n\n");
+  console.log(gazeData);
+  sendDataToServer();
+}
+
+async function sendDataToServer() {
+  const url = "http://localhost:3000/api/gaze";
+
+  const data = { gazeData: gazeData };
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Server response:", data);
+      console.log("Data Recived Sucessfully ");
+    })
+    .catch((error) => {
+      console.error("Error during POST request:", error);
+    });
+}
+
+function showHeatMap() {
+  visibleMap = true;
+  const allElements = document.querySelectorAll("*");
+  for (const element of allElements) {
+    if (
+      element.tagName != "HTML" &&
+      element.tagName != "BODY" &&
+      element.tagName != "HEAD" &&
+      element.tagName != "SCRIPT" &&
+      element.getAttribute("id") != "my_dataviz"
+    ) {
+      element.style.display = "none";
+    }
+  }
+  // width = `${window.visualViewport.width}px`;
+  // height = `${window.visualViewport.height}px`;
+  var margin = {top: 20, right: 30, bottom: 30, left: 30},
+    width = window.visualViewport.width - margin.left - margin.right,
+    height = window.visualViewport.height - margin.top - margin.bottom;
+
+  var svg = d3
+    .select("#my_dataviz")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  // .attr("padding", 0)
+  // .attr("margin", 0)
+
+  d3.csv(
+    "http://localhost:3000/api/gaze",
+    function (data) {
+      // Get max and min of data
+      var xLim = [4, 18];
+      var yLim = [6, 20];
+
+      // Add X axis
+      var x = d3.scaleLinear().nice().domain(xLim).range([0, width]);
+      svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+      // Add Y axis
+      var y = d3.scaleLinear().nice().domain(yLim).range([height, 0]);
+      svg.append("g").call(d3.axisLeft(y));
+
+      // Reformat the data: d3.rectbin() needs a specific format
+      var inputForRectBinning = [];
+      data.forEach(function (d) {
+        inputForRectBinning.push([+d.x, +d.y]); // Note that we had the transform value of X and Y !
       });
+
+      // Compute the rectbin
+      var size = 0.5;
+      var rectbinData = d3.rectbin().dx(size).dy(size)(inputForRectBinning);
+
+      // Prepare a color palette
+      var color = d3
+        .scaleLinear()
+        .domain([0, 350]) // Number of points in the bin?
+        .range(["transparent", "#69a3b2"]);
+
+      // What is the height of a square in px?
+      heightInPx = y(yLim[1] - size);
+
+      // What is the width of a square in px?
+      var widthInPx = x(xLim[0] + size);
+
+      // Now we can add the squares
+      svg
+        .append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+      svg
+        .append("g")
+        .attr("clip-path", "url(#clip)")
+        .selectAll("myRect")
+        .data(rectbinData)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {
+          return x(d.x);
+        })
+        .attr("y", function (d) {
+          return y(d.y) - heightInPx;
+        })
+        .attr("width", widthInPx)
+        .attr("height", heightInPx)
+        .attr("fill", function (d) {
+          return color(d.length);
+        })
+        .attr("stroke", "black")
+        .attr("stroke-width", "0.4");
+    }
+  );
+}
+window.addEventListener("resize", () => {
+  if (visibleMap) {
+    showHeatMap();
   }
 });
